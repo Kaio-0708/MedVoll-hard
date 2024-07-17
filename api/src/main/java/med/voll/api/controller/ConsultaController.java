600c7a6f348f19d5c9be8e7bbb0448f1a9eacf35
Kaio0708 <kaiovitor0707@gmail.com>
2024-07-16 22:02:18 -0300

    first commit

---
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsulta;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+//@SecurityRequirement(name = "bearer-key")
+public class ConsultaController {
+
+    @Autowired
+    public AgendaDeConsulta agenda;
+
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
+        var dto = agenda.agendar(dados);
+        return ResponseEntity.ok(dto);
+    }
+}
